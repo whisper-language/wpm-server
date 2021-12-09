b639a53f922995577ff31847d55ba162733b4da1
jzx <945627077@qq.com>
2021-12-09 10:35:48 +0800

    '增加rucket测试'

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+
+#[get("/")]
+fn index() -> &'static str {
+    "wpm-server"
+}
+
+#[get("/version")]              // <- route attribute
+fn version() -> &'static str {  // <- request handler
+    "0.0.1"
+}
+
+
+#[derive(Responder)]
+#[response(status = 418, content_type = "json")]
+struct RawTeapotJson(&'static str);
+
+#[get("/search/<words>")]
+fn search(words: String) -> RawTeapotJson {
+    RawTeapotJson("[{},{}]")
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index,search])
 }
